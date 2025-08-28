cdc0e29980dfd5332e456d3206470d1af283997e
YukiZenin <109053214+YukiZenin@users.noreply.github.com>
2025-08-28 10:52:39 -0500

    Version_2_ExpresioneslambdacomoExpresiones

---
+++ UI.NumerosAleatorios/frmDatosAleatorios.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace UI.NumerosAleatorios
+{
+    partial class frmDatosAleatorios
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnMostrarMensaje = new Button();
+            SuspendLayout();
+            // 
+            // btnMostrarMensaje
+            // 
+            btnMostrarMensaje.Location = new Point(425, 204);
+            btnMostrarMensaje.Name = "btnMostrarMensaje";
+            btnMostrarMensaje.Size = new Size(137, 29);
+            btnMostrarMensaje.TabIndex = 0;
+            btnMostrarMensaje.Text = "MostrarMensaje";
+            btnMostrarMensaje.UseVisualStyleBackColor = true;
+            btnMostrarMensaje.Click += btnMostrarMensaje_Click;
+            // 
+            // frmDatosAleatorios
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnMostrarMensaje);
+            Name = "frmDatosAleatorios";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnMostrarMensaje;
+    }
+}
---
+++ UI.NumerosAleatorios/frmDatosAleatorios.cs
@@ -0,0 +1,15 @@
+namespace UI.NumerosAleatorios
+{
+    public partial class frmDatosAleatorios : Form
+    {
+        public frmDatosAleatorios()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrarMensaje_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola mundo");
+        }
+    }
+}
